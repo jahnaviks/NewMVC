ab3df2059d810df8325835fc119346db176bbc7c
Karnam Sreedhar Jahnavi <jahnavis@Virtusa.com>
2019-01-21 15:14:07 +0530

    First commit Message

---
+++ dbo.Procedure.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[Procedure]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
+
+
+
+CREATE PROCEDURE RegisterQueries(
+ IN orderStatus VARCHAR(25),
+ OUT total INT)
+BEGIN
+ SELECT count(orderNumber)
+ INTO total
+ FROM orders
+ WHERE status = orderStatus;
+END
